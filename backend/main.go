d1c775702f48284f2c1590d3dcc30d1341f0b0c1
Liuyifan <0219lyf@gmail.com>
2025-07-18 10:18:58 +0800

    middleware CORS and docker-compose to build mysql

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"strings"
+	"time"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/yifaaan/webook/internal/web"
@@ -10,11 +13,14 @@ func main() {
 	server := gin.Default()
 
 	server.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
-		AllowMethods:     []string{"POST", "GET", "PUT", "DELETE", "OPTIONS"},
+		// AllowOrigins: []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "OPTIONS", "DELETE"},
 		AllowHeaders:     []string{"Content-Type", "Authorization"},
-		ExposeHeaders:    []string{"x-jwt-token", "x-refresh-token"},
-		AllowCredentials: true,
+		AllowCredentials: true, // 是否允许携带cookie
+		AllowOriginFunc: func(origin string) bool {
+			return strings.Contains(origin, "localhost") || strings.Contains(origin, "127.0.0.1")
+		},
+		MaxAge: 12 * time.Hour,
 	}))
 	u := web.NewUserHandler()
 	u.RegisterRoutes(server)
