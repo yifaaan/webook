f4b520438cd61c431fc2fd156c58e712f91914c3
yifaaan <0219lyf@gmail.com>
2025-07-19 09:19:06 +0800

    refactor main

--- backend/main.go
+++ backend/main.go
@@ -15,14 +15,34 @@ import (
 )
 
 func main() {
+	db := initDB()
+	server := initWebServer()
+	u := initUser(db)
+	u.RegisterRoutes(server)
+	server.Run(":8080")
+}
+
+func initDB() *gorm.DB {
 	db, err := gorm.Open(mysql.Open("root:123456@tcp(localhost:3306)/webook"))
 	if err != nil {
 		panic(err)
 	}
+	err = dao.InitTable(db)
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
+
+func initUser(db *gorm.DB) *web.UserHandler {
 	d := dao.NewUserDAO(db)
 	repo := repository.NewUserRepository(d)
 	svc := service.NewUserService(repo)
+	u := web.NewUserHandler(svc)
+	return u
+}
 
+func initWebServer() *gin.Engine {
 	server := gin.Default()
 	// 跨域middleware
 	server.Use(cors.New(cors.Config{
@@ -35,11 +55,5 @@ func main() {
 		},
 		MaxAge: 12 * time.Hour,
 	}))
-
-	u := web.NewUserHandler(svc)
-	u.RegisterRoutes(server)
-
-	// REST
-	// server.PUT("/user/", func(ctx *gin.Context) {})
-	server.Run(":8080")
+	return server
 }
